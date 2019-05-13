3b868821406b65ac77cde4a05942f8f1bb5f5891
Jonathan McDonald <jonathanlmcd@gmail.com>
2019-05-13 16:38:15 -0600

    basic redux setup

---
+++ src/ducks/reducer.js
@@ -0,0 +1,17 @@
+const initialState = {
+
+}
+
+// Consts
+
+
+// Action Creators
+
+
+// Reducer
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
